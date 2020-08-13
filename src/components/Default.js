d0ce41cebd1f35fe04a2f5ff0724d7aeb1042edb
isamintha <33679829+isamintha@users.noreply.github.com>
2020-08-13 18:46:33 +0530

    first commit

---
+++ src/components/Default.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Default extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-uppercase pt-5 text-title">
+            <h2 className="display-3">404</h2>
+            <h2>error</h2>
+            <h2>page not found</h2>
+            <h4>
+              the requested URL
+              <span className="text-danger">
+                {this.props.location.pathname}
+              </span>{" "}
+              was not found
+            </h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Default;
